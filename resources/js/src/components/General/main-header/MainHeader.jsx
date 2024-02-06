866684f59505756ac7a5e1c54832caa3f4ab752b
mabozed <mabozed.com@gmail.com>
2024-02-07 00:11:56 +0300

    UI Notes

--- resources/js/src/components/General/main-header/MainHeader.jsx
+++ resources/js/src/components/General/main-header/MainHeader.jsx
@@ -3,8 +3,8 @@ const MainHeader = ({ firstTitle, secondTitle }) => {
     return (
         <div className="main-header">
             <h1>
-                {firstTitle}
-                <span>{secondTitle}</span>
+                <span className="main-header-first">{firstTitle}</span>
+                <span className="main-header-last">{secondTitle}</span>
             </h1>
         </div>
     );
